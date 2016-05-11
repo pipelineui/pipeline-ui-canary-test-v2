4b103389478a50dbc54751731dca9e59bb6c7d73
pipelineui <pipelineui@gmail.com>
2016-05-11 09:47:30 -0400

    Create spec.js

---
+++ spec.js
@@ -0,0 +1,18 @@
+var request = require("supertest");
+
+var appName = process.env.IDS_PROJECT_NAME.split("|").pop().replace(" ", "");
+var url = "http://" + appName + ".mybluemix.net";
+
+console.log("appName: " + process.env.IDS_PROJECT_NAME);
+console.log("Request url: " + url);
+
+request = request(url);
+
+describe("GET /", function () {
+    it("respond with html", function (done) {
+        request
+            .get("/")
+            .expect("Content-Type", /html/)
+            .expect(200, done);
+    });
+});
