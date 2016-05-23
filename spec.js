c742abf5f241ec3047b2378771f2244e3d043ec7
pipelineui <pipelineui@gmail.com>
2016-05-23 08:41:06 -0400

    Update spec.js

--- spec.js
+++ spec.js
@@ -1,17 +1,8 @@
-var request = require("supertest");
+var expect = require('chai').expect;
 
-var appName = process.env.IDS_PROJECT_NAME.split("|").pop().replace(" ", "");
-var url = "http://" + appName + ".mybluemix.net";
-
-console.log("Request url: " + url);
-
-request = request(url);
-
-describe("GET /", function () {
-    it("respond with html", function (done) {
-        request
-            .get("/")
-            .expect("Content-Type", /html/)
-            .expect(200, done);
+// Dummy mocha test for canary test
+describe("Dummy test", function () {
+    it("should always pass", function() {
+        expect(true).to.equal(true);
     });
 });
